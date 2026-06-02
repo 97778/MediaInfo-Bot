d823f358fa558c81c20b99e5caa6e784a4cf1679
izaz97778 <izaz97778@gmail.com>
2026-06-02 19:43:03 +0500

    Add MONGO_URI and set default values for IDs
    Added MONGO_URI for MongoDB setup in bot.py and set default values for API_ID and ADMIN_ID.

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", ))
+API_ID = int(os.environ.get("API_ID", "0"))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-ADMIN_ID = int(os.environ.get("ADMIN_ID", ))
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "0"))
+
+# Added for MongoDB setup in bot.py
+MONGO_URI = os.environ.get("MONGO_URI", "")
 
 _raw = os.environ.get("ALLOWED_CHATS", "")
 ALLOWED_CHATS = [int(x.strip()) for x in _raw.split(",") if x.strip()]
